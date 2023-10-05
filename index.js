19f7b3e7803e35cc64bdca9290b8d7c70d6202c5
riot-HX <konstantinos.karachristos@holidayextras.com>
2023-10-05 13:10:32 +0300

    only promises

--- index.js
+++ index.js
@@ -1,63 +1,32 @@
-const { Worker } = require('worker_threads')
-
-
-const workers = []
 
 async function asyncQueue(tasks, maxWorkers = 4) {
-    let allTasks
-    Array(maxWorkers).fill().forEach(() => {
-        workers.push({
-            worker: new Worker('./worker.js'),
-            isFree: true
+
+    const runTask = async (task) => {
+        if (!maxWorkers) return new Promise(resolve => setImmediate(() => resolve(runTask(task))))
+
+        maxWorkers--
+        return task().then(res => {
+            maxWorkers++
+            return res
         })
+    }
+
+    const results = tasks.map((task) => {
+        return runTask(task)
     })
-    const findFreeWorker = async () => {
-        const index = workers.findIndex(worker => worker.isFree === true)
-        if (index === -1) {
-            await new Promise(resolve => setTimeout(resolve, 10))
-            return findFreeWorker()
-        }
-        workers[index].isFree = false
-        return index
-    }
-    const taskToPromise = (task, index) => {
-        return new Promise(async (resolve, reject) => {
-            const workerIndex = await findFreeWorker()
-            workers[workerIndex].worker.postMessage({
-                func: task.toString().replace(/${value}|value/g, index + 1)
-            })
-            workers[workerIndex].worker.on('message', (msg) => {
-                workers[workerIndex].isFree = true
-                resolve(msg)
-            })
-            workers[workerIndex].worker.on('error', (msg) => {
-                workers[workerIndex].isFree = true
-                
-                resolve(msg)
-            })
-            workers[workerIndex].worker.on('exit', code => {
-                if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`))
-            })
-        })
-    }
 
-    allTasks = tasks.map((task, index) => taskToPromise(task, index))
-
-    const results = (await Promise.allSettled(allTasks)).map(res => res.value)
-    workers.forEach(worker => worker.worker.terminate())
-    return results
+    return Promise.all(results)
 }
   
 const tasksToRun = [1, 2, 3, 4, 5, 6].map(
-    (value) => () => {
-        return new Promise((resolve) => {
-        console.log(`Starting task ${value}...`)
-        setTimeout(() => {
-            console.log(`Task ${value} finished...`)
-            resolve(value)
-        }, value * 1000)
-    })}
-)
-
+    (value) => () =>
+        new Promise((resolve) => {
+            console.log(`Starting task ${value}...`)
+            setTimeout(() => {
+                console.log(`Task ${value} finished...`)
+                resolve(value)
+            }, value * 1000)
+        })
+);
   
-asyncQueue(tasksToRun).then(res => console.log('res', res))
+asyncQueue(tasksToRun).then(console.log)
--- worker.js
+++
@@ -1,6 +0,0 @@
-const { parentPort } = require('worker_threads')
-
-parentPort.on('message', (msg) => {
-    if (msg === 'kill') process.exit()
-    const func = (new Function('return (' + msg.func + ')')())().then(res => parentPort.postMessage(res))
-})
